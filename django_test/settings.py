018082c0e5c83e204d7c9c080e8390ee6fcab652
unknown <zaman_shovon>
2021-02-25 16:04:10 +0600

    profile image, validation and update done

--- django_test/settings.py
+++ django_test/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,6 +119,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+MEDIA_URL = '/media/'
+
 STATIC_URL = '/static/'
 CRISPY_TEMPLATE_PACK='Bootstrap4'
-LOGIN_REDIRECT_URL = 'blog-home'
+LOGIN_REDIRECT_URL = 'profile'
--- users/views.py
+++ users/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
-from django.shortcuts import redirect
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from .forms import UserRegistrationForm 
+from .forms import UserRegistrationForm, UserUpdateForm, UserImageUpdate 
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 def register(request):
     if request.method == "POST":
@@ -15,3 +15,29 @@ def register(request):
     else:
         form = UserRegistrationForm()
     return render(request,'user/register.html',{'form':form})
+
+@login_required
+def profile(request):
+
+    if request.method == 'POST' :
+        user_field_form = UserUpdateForm(request.POST, instance = request.user)
+        user_image_update = UserImageUpdate(request.POST,
+                                            request.FILES, 
+                                            instance = request.user.profile)
+
+        if user_field_form.is_valid() and user_image_update.is_valid():
+            user_field_form.save()
+            user_image_update.save()
+            messages.success(request, f'Your profile successfully updated!')
+            return redirect('profile')
+        else:
+            messages.warning(request, f'Wrong input fields! Try again!')
+            return redirect('profile')
+    else:
+        user_field_form = UserUpdateForm
+        user_image_update = UserImageUpdate
+    context = {
+        'u_form': user_field_form,
+        'p_form': user_image_update
+    }
+    return render(request, 'user/profile.html', context)
